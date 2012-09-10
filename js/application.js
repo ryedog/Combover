2dc4f8c67801282d0e186be349f7ea99808b5fed
Austin Cornelio <austin@yhmg.com>
2012-09-10 15:09:08 -0400

    more combover marketing page

--- js/application.js
+++ js/application.js
@@ -10,41 +10,66 @@ Combover_playgound = (function ($) {
     view = {
         'template': 'textarea#textarea-html-template',
         'object': 'textarea#textarea-json-object',
-        'compile_button': 'button#button-compile'
+        'compile_button': 'button#button-compile',
+        'reset': 'a#try-again',
+        'result': 'div#result'
     },
 
     controller = {
         compile: function() {
-            var json;
+            var json, template, reset;
             json = $(view.object).val();
 
             // Set the tempate and set the JSON obj
             model.html_template = $(view.template).val();
             model.JSON_object = $.parseJSON(json);
 
-            // Combover Render
-            //$('#result').html(model.html_template(model.JSON_object));
+            Combover.debug = true;
 
-            // Compile
-            //$(model.html_template) = Combover.compile('#result');
+            template = Combover.compile(model.html_template);
 
-            /* --------- debigging --------- */
-            console.log('--- template ---');
-            console.log(model.html_template);
+            $(view.result).html(template(model.JSON_object));
 
-            console.log('--- object ---');
-            console.log(model.JSON_object);
+            $(view.reset).removeClass('hidden');
+            
+        }, 
+
+        resetResultUI: function(e) {
+            
+            e.preventDefault();
+
+            var template, json, code, trigger
+
+            template = '<div><div comb="hairspray"></div></div>';
+            json = '{"hairspray": "extra hold"}';
+
+            //1. Reset HTML
+            $(view.template).val(template);
+
+            //2. Reset JSON
+            $(view.object).val(json);
+
+            //3. Reset Result
+            $(view.result).html('');
+
+            //4. Show compile button
+            $(view.result).append('<button id="button-compile"class="btn">Compile Result</button>');
+
+            //5. Hide try again
+            $(view.reset).addClass('hidden');
+
+
         }
     },
 
     eventBinder = function() {
-        $(view.compile_button).on('click' , controller.compile);
+        $('.code').delegate(view.compile_button,'click' , controller.compile);
+        $('.sample-code').delegate('a#try-again', 'click', controller.resetResultUI);
     };
 
     return {
         init: function() {
             eventBinder();
-
         }
     }
 
